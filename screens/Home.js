55a935af79b222226ed5f0b5cc9b25ce4a8d6684
jlfrances <joseluis.francesverlini@gmail.com>
2022-12-26 16:36:05 -0300

    Add movies lists + generic error messages (42)

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Dimensions, ScrollView} from 'react-native';
+import {
+  ActivityIndicator,
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+import {SliderBox} from 'react-native-image-slider-box';
 import {
   getPopularMovies,
   getUpcomingMovies,
@@ -7,19 +14,20 @@ import {
   getFamilyMovies,
   getDocumentaries,
 } from '../services/services';
-import {SliderBox} from 'react-native-image-slider-box';
 import List from '../components/List';
+import Error from '../components/Error';
 
 const dimensions = Dimensions.get('screen');
 
 const Home = () => {
   const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [upcomingMoviesImages, setUpcomingMoviesImages] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
   const [familyMovies, setFamilyMovies] = useState([]);
   const [documentaries, setDocumentaries] = useState([]);
   const [popularTV, setPopularTV] = useState([]);
-  const [images, setImages] = useState([]);
   const [error, setError] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const getData = () => {
     return Promise.all([
@@ -47,7 +55,7 @@ const Home = () => {
               `https://image.tmdb.org/t/p/w500${mov.poster_path}`,
             );
           });
-          setImages(moviesImages.slice(0, 10));
+          setUpcomingMoviesImages(moviesImages.slice(0, 10));
 
           setUpcomingMovies(upcomingMoviesData.slice(0, 10));
           setPopularMovies(popularMoviesData.slice(0, 10));
@@ -59,48 +67,56 @@ const Home = () => {
       .catch(err => {
         setError(err);
         console.log('error', err);
+      })
+      .finally(() => {
+        setIsLoaded(true);
       });
   }, []);
 
   return (
     <React.Fragment>
-      <ScrollView>
-        {images && (
-          <View style={styles.sliderContainer}>
-            <SliderBox
-              images={images}
-              autoplay={true}
-              circleLoop={true}
-              dotStyle={styles.sliderStyle}
-              sliderBoxHeight={dimensions.height / 1.5}
-            />
-          </View>
-        )}
+      {isLoaded && !error && (
+        <ScrollView>
+          {upcomingMoviesImages && (
+            <View style={styles.sliderContainer}>
+              <SliderBox
+                images={upcomingMoviesImages}
+                autoplay={true}
+                circleLoop={true}
+                dotStyle={styles.sliderStyle}
+                sliderBoxHeight={dimensions.height / 1.5}
+              />
+            </View>
+          )}
 
-        {popularMovies && (
-          <View style={styles.sliderContainer}>
-            <List title="Popular movies" content={popularMovies} />
-          </View>
-        )}
+          {popularMovies && (
+            <View style={styles.sliderContainer}>
+              <List title="Popular movies" content={popularMovies} />
+            </View>
+          )}
 
-        {popularTV && (
-          <View style={styles.sliderContainer}>
-            <List title="Popular TV shows" content={popularTV} />
-          </View>
-        )}
+          {popularTV && (
+            <View style={styles.sliderContainer}>
+              <List title="Popular TV shows" content={popularTV} />
+            </View>
+          )}
 
-        {familyMovies && (
-          <View style={styles.sliderContainer}>
-            <List title="Family movies" content={familyMovies} />
-          </View>
-        )}
+          {familyMovies && (
+            <View style={styles.sliderContainer}>
+              <List title="Family movies" content={familyMovies} />
+            </View>
+          )}
 
-        {documentaries && (
-          <View style={styles.sliderContainer}>
-            <List title="Documentaries" content={documentaries} />
-          </View>
-        )}
-      </ScrollView>
+          {documentaries && (
+            <View style={styles.sliderContainer}>
+              <List title="Documentaries" content={documentaries} />
+            </View>
+          )}
+        </ScrollView>
+      )}
+
+      {!isLoaded && <ActivityIndicator size="large" />}
+      {error && <Error />}
     </React.Fragment>
   );
 };
